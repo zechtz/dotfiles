a95bf6679ba88355011035a1c5bfc85215ef2dc8
Zachariah Ngonyani <zachariahn@gmail.com>
2021-12-13 11:15:16 +0300

    start moving to init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+require("settings")
+require("keymappings")
+
+require("plugins.go")
+require("plugins.airline")
+require("plugins.config.floaterm")
+
+-- Use Plug to manage plugins
+vim.call('plug#begin', '~/.config/nvim/plugged')
+--color schemes
+require("colorschemes")
+-- vim utils move to own file as well
+require("utils")
+-- syntax plugins - move to own file
+require("syntax")
+vim.call('plug#end')
+
+require("colors.onedark")
+require("colors.dark")
