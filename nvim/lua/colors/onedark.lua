be41cb952fc7cc4994630723ea24d08ff068b50a
Zachariah Ngonyani <zachariahn@gmail.com>
2021-12-13 22:45:48 +0300

    create configuration for one dark colorscheme

---
+++ nvim/lua/colors/onedark.lua
@@ -0,0 +1,11 @@
+vim.cmd([[
+colorscheme onedark
+let base16colorspace=256
+]])
+
+vim.g.onedark_termcolors=256
+
+vim.g.onedark_hide_endofbuffer=1
+vim.g.onedark_terminal_italics=1
+
+vim.g.lightline = { colorscheme = 'onedark'}
