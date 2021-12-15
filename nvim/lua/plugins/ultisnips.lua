4694036bcda961b703a1976cd4cef3c94bf08101
Zachariah Ngonyani <zachariahn@gmail.com>
2021-12-15 07:14:07 +0300

    remove unused plugins

--- nvim/lua/plugins/ultisnips.lua
+++
@@ -1,6 +0,0 @@
---" Ultisnips
---" Trigger configuration. Do not use <tab> if you use https://github.com/Valloric/YouCompleteMe.
-vim.g.UltiSnipsExpandTrigger="<Leader>r"
-vim.g.UltiSnipsJumpForwardTrigger="<Leader>f"
-vim.g.UltiSnipsJumpBackwardTrigger="<Leader>b"
-vim.g.UltiSnipsEditSplit="vertical"
--- nvim/lua/plugins/vsnip.lua
+++
@@ -1,5 +0,0 @@
--- NOTE: You can use other key to expand snippet.
--- If you want to use snippet for multiple filetypes, you can `g:vsnip_filetypes` for it.
-vim.g.vsnip_filetypes = {}
-vim.g.vsnip_filetypes.javascriptreact = 'javascript'
-vim.g.vsnip_filetypes.typescriptreact = 'typescript'
