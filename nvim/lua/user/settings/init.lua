93e58b43c25cabc23cdaa8807174e7076eebc5e0
Zachariah Ngonyani <zachariahn@gmail.com>
2021-12-23 08:48:28 +0300

    rename nvim directory

--- nvim/lua/user/settings/init.lua
+++
@@ -1,2 +0,0 @@
-require("user.settings.keymapping")
-require("user.settings.airline")
--- nvim/lua/user/themes/tokyoday.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd([[
- set termguicolors
- colorscheme tokyonight
-]])
-
-vim.g.tokyonight_style = 'day'
-vim.g.tokyonight_enable_italic = 1
