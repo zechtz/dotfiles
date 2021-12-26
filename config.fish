2d330a7aba160d75c17c651cf38a42e1fe3015e2
Zachariah Ngonyani <zachariahn@gmail.com>
2021-12-26 11:38:46 +0300

    fix goroot

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 source ~/.asdf/asdf.fish
 
 set -x JAVA_HOME /usr/bin
-set -gx GOROOT /usr/local/go/bin/go
+set -gx GOROOT /usr/local/go
 set -gx GOPATH /data/go/src
 
-set -gx PATH /home/mtabe/.asdf/bin $PATH
+set -gx PATH /home/mtabe/.asdf/bin  $GOROOT/bin $PATH
