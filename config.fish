225667620e2cc886d208cbb0449fb79cfb7c930d
Zachariah Ngonyani <zachariahn@gmail.com>
2021-12-27 10:32:36 +0300

    add scripts directory to path

--- config.fish
+++ config.fish
@@ -4,5 +4,6 @@ set -gx JAVA_HOME /usr/bin
 set -gx GOROOT /usr/local/go
 set -gx GOPATH /data/go/src
 set -gx CARGOPATH  /home/mtabe/.cargo
+set -gx SCRIPTS /data/work/dotfiles/scripts
 
-set -gx PATH /home/mtabe/.asdf/bin $CARGOPATH/bin $GOROOT/bin $PATH
+set -gx PATH /home/mtabe/.asdf/bin $CARGOPATH/bin $GOROOT/bin $SCRIPTS $PATH
