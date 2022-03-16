d17015e4794ee23cba04892c57f5caed3a98aa29
Zachariah Ngonyani <zachariahn@gmail.com>
2022-03-16 11:01:07 +0300

    refactor null-ls

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -3,23 +3,6 @@ if not null_ls_status_ok then
 	return
 end
 
-local filetypes = {
-	"javascript",
-	"typescript",
-	"javascriptreact",
-	"typescriptreact",
-	"css",
-	"scss",
-	"less",
-	-- "html",
-	-- "json",
-	"yaml",
-	"markdown",
-	"graphql",
-	"solidity",
-	"vue",
-}
-
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
@@ -28,20 +11,9 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({
-			filetypes = filetypes,
-			extra_filetypes = { "toml", "solidity" },
-			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-		}),
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-		-- formatting.mix,
-		diagnostics.phpcs,
-		diagnostics.misspell,
+		diagnostics.flake8,
 	},
-	on_attach = function(client)
-		if client.resolved_capabilities.document_formatting then
-			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-		end
-	end,
 })
