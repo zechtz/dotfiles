6bea82b368561c08c767870b710ba7f5adc19d9d
Zachariah Ngonyani <zachariahn@gmail.com>
2022-09-21 08:05:23 +0300

    remove unecessary comments

--- nvim/lua/user/copilot/init.lua
+++ nvim/lua/user/copilot/init.lua
@@ -1,23 +1,10 @@
--- For copilot.vim
--- vim.g.copilot_filetypes = {
---   ["*"] = false,
--- }
-
--- vim.cmd [[
---   imap <silent><script><expr> <C-A> copilot#Accept("\<CR>")
---   let g:copilot_no_tab_map = v:true
--- ]]
-
 local status_ok, copilot = pcall(require, "copilot")
 if not status_ok then
   return
 end
 
 copilot.setup {
-  cmp = {
-    enabled = true,
-    method = "getCompletionsCycling",
-  },
+  cmp = { enabled = true, method = "getCompletionsCycling" },
   panel = { -- no config options yet
     enabled = true,
   },
