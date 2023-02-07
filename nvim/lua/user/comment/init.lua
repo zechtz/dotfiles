a63a0f8269366c1224b447835d4d9848c85d1ee8
Zachariah Ngonyani <zachariahn@gmail.com>
2023-02-07 03:25:36 +0300

    fix comments for react tsx

--- nvim/lua/user/comment/init.lua
+++ nvim/lua/user/comment/init.lua
@@ -15,27 +15,18 @@ comment.setup {
     local line_start = (ctx.srow or ctx.range.srow) - 1
     local line_end = ctx.erow or ctx.range.erow
     require("lsp-inlayhints.core").clear(0, line_start, line_end)
+    local U = require "Comment.utils"
 
-    require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
-
-    if vim.bo.filetype == "javascript" or vim.bo.filetype == "typescript" then
-      local U = require "Comment.utils"
-
-      -- Determine whether to use linewise or blockwise commentstring
-      local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
-
-      -- Determine the location where to calculate commentstring from
-      local location = nil
-      if ctx.ctype == U.ctype.blockwise then
-        location = require("ts_context_commentstring.utils").get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = require("ts_context_commentstring.utils").get_visual_start_location()
-      end
-
-      return require("ts_context_commentstring.internal").calculate_commentstring {
-        key = type,
-        location = location,
-      }
+    local location = nil
+    if ctx.ctype == U.ctype.block then
+      location = require("ts_context_commentstring.utils").get_cursor_location()
+    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+      location = require("ts_context_commentstring.utils").get_visual_start_location()
     end
+
+    return require("ts_context_commentstring.internal").calculate_commentstring {
+      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+      location = location,
+    }
   end,
 }
