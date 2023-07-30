a8820244bca1c10dcb5f5fe4a77dd382c5632f0e
Zachariah Ngonyani <zachariahn@gmail.com>
2023-07-30 04:24:26 +0300

    feat: update options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,5 +5,7 @@
 vim.opt.showtabline = 0 -- always show tabs
 vim.opt.undolevels = 10000 -- How many undos
 vim.opt.undoreload = 10000 -- number of lines to save for undo
+vim.opt.conceallevel = 3
 
+vim.cmd([[set iskeyword+=-]])
 vim.api.nvim_set_hl(0, "Comment", { italic = true })
