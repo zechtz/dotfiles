2a98ae562133f49e8b6ffbd0876acf4d7a63ea85
Zachariah Ngonyani <zachariahn@gmail.com>
2023-08-04 00:19:09 +0300

    fix: properly configure nvim-cmp

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,14 +1,182 @@
+local check_backspace = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local buffer_fts = { "markdown", "toml", "yaml", "json" }
+
+local function contains(t, value)
+  for _, v in pairs(t) do
+    if v == value then
+      return true
+    end
+  end
+  return false
+end
+
+local compare = require("cmp.config.compare")
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
 return {
   "hrsh7th/nvim-cmp",
-  -- load cmp on InsertEnter
-  event = "InsertEnter",
-  -- these dependencies will only be loaded when cmp loads
-  -- dependencies are always lazy-loaded unless specified otherwise
   dependencies = {
+    "hrsh7th/cmp-emoji",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "saadparwaiz1/cmp_luasnip",
+    -- Add other sources you want to use here
   },
-  config = function()
-    -- ...
+
+  opts = function()
+    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local defaults = require("cmp.config.default")()
+    local tabnine = require("cmp_tabnine.config")
+
+    return {
+      completion = {
+        completeopt = "menu,menuone,noinsert",
+      },
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+        ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<m-o>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<C-c>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+        ["<m-j>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+        ["<m-k>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+        ["<m-c>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+        ["<S-CR>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+        -- Accept currently selected item. If none selected, `select` first item.
+        -- Set `select` to `false` to only confirm explicitly selected items.
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Right>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.jumpable(1) then
+            luasnip.jump(1)
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif luasnip.expandable() then
+            luasnip.expand()
+          elseif check_backspace() then
+            -- cmp.complete()
+            fallback()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = "crates", group_index = 1 },
+        {
+          name = "nvim_lsp",
+          filter = function(entry, ctx)
+            local kind = require("cmp.types.lsp").CompletionItemKind[entry:get_kind()]
+            if kind == "Snippet" and ctx.prev_context.filetype == "java" then
+              return true
+            end
+
+            if kind == "Text" then
+              return true
+            end
+          end,
+          group_index = 2,
+        },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "luasnip", group_index = 2 },
+        {
+          name = "buffer",
+          group_index = 2,
+          filter = function(entry, ctx)
+            if not contains(buffer_fts, ctx.prev_context.filetype) then
+              return true
+            end
+          end,
+        },
+        { name = "cmp_tabnine", group_index = 2 },
+        { name = "path", group_index = 2 },
+        { name = "emoji", group_index = 2 },
+        { name = "lab.quick_data", keyword_length = 4, group_index = 2 },
+      }),
+      formatting = {
+        format = function(_, item)
+          local icons = require("lazyvim.config").icons.kinds
+          if icons[item.kind] then
+            item.kind = icons[item.kind] .. item.kind
+          end
+          return item
+        end,
+      },
+      experimental = {
+        ghost_text = {
+          hl_group = "CmpGhostText",
+        },
+      },
+      sorting = {
+        priority_weight = 2,
+        comparators = {
+          -- require("copilot_cmp.comparators").prioritize,
+          -- require("copilot_cmp.comparators").score,
+          compare.offset,
+          compare.exact, -- compare.scopes,
+          compare.score,
+          compare.recently_used,
+          compare.locality, -- compare.kind,
+          compare.sort_text,
+          compare.length,
+          compare.order,
+          -- require("copilot_cmp.comparators").prioritize,
+          -- require("copilot_cmp.comparators").score,
+        },
+      },
+
+      confirm_opts = { behavior = cmp.ConfirmBehavior.Replace, select = false },
+      window = {
+        documentation = false,
+        -- documentation = {
+        -- border = "rounded",
+        -- winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+        -- },
+        completion = {
+          border = "rounded",
+          winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+        },
+      },
+    },
+      tabnine:setup({
+        -- Your TabNine configuration options here...
+        max_lines = 1000,
+        max_num_results = 20,
+        sort = true,
+        run_on_every_keystroke = true,
+        snippet_placeholder = "..",
+        ignored_file_types = {
+          -- default is not to ignore
+          -- uncomment to ignore in lua:
+          -- lua = true
+        },
+        show_prediction_strength = false,
+      })
   end,
 }
