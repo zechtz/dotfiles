96e8311e136713bfe83cbf31ce258c166e16a4b0
Zachariah Ngonyani <zachariahn@gmail.com>
2024-06-12 06:54:16 +0300

    minor: update packages

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -145,6 +145,7 @@ return {
     servers = {
       jsonls = {},
       jdtls = {},
+      tailwindcss = {},
       lua_ls = {
         -- mason = false, -- set to false if you don't want this server to be installed with mason
         -- Use this to add any additional keymaps
