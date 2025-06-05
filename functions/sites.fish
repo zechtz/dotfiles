4cd90a38103047c14a854c2eb1335fc22c725d09
Zachariah Ngonyani <zachariahn@gmail.com>
2025-06-05 06:35:01 +0300

    fix: change config to reflect mbp paths

--- functions/sites.fish
+++ functions/sites.fish
@@ -1,4 +1,5 @@
 # Defined in - @ line 1
-function sites --wraps='cd /data/sites' --description 'alias sites=cd /data/sites'
-  cd /data/sites $argv;
+function sites --wraps='cd /Volumes/Work/sites' --description 'alias sites=cd /Volumes/Work/sites'
+    cd /Volumes/Work/sites $argv
+
 end
