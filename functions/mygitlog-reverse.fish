b34f532ce53179673c317621b4f7766b0162e213
Zachariah Ngonyani <zachariahn@gmail.com>
2025-12-01 07:51:16 +0300

    feat: add another git alias

--- functions/mygitlog-reverse.fish
+++ functions/mygitlog-reverse.fish
@@ -1,7 +1,7 @@
 function mygitlog-reverse
     set author (test -n "$argv[1]" && echo $argv[1] || git config user.name)
     # Get the list of commit hashes for the author in reverse order
-    set commits (git log --oneline --author="$author" --reverse | awk '{print $1}')
+    set commits (git log --oneline --author="$author" --reverse --no-merges | awk '{print $1}')
     if test -n "$commits"
         echo "Commits by $author (oldest first):"
         for commit in $commits
